8cc0f97e404d780c7fd5a5f02376610e30ee82a9
MUHAMMAD <mohamadnematpoor@gmail.com>
2021-02-09 15:45:52 +0330

    second commit

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Job(models.Model):
+    imgae = models.ImageField(upload_to="images/")
+    summary = models.CharField(max_length=250, blank=True)
+
+    def __str__(self):
+        return self.summary
