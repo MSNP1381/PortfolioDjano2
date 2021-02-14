c684ab0d42cb95bda77faef0592e69cb0cd2439f
MUHAMMAD <mohamadnematpoor@gmail.com>
2021-02-14 18:29:00 +0330

    bootstrap

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
     'jobs.apps.JobsConfig',
     'blog_app.apps.BlogAppConfig',
     'django.contrib.admin',
--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Blog_model(models.Model):
+    title=models.CharField(max_length=200)
+    image=models.ImageField(upload_to="images/")
+    body=models.TextField(blank=True)
+    pub_date=models.DateField(auto_now_add=True)
