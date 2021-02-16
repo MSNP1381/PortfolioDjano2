9115ed9fb236b791f4794ed87b5a38f3efa5233e
MUHAMMAD <mohamadnematpoor@gmail.com>
2021-02-17 01:41:44 +0330

    postgres generated

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'Portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': '@msnp1381',
+        'HOST': 'localhost',
+        'PORT':'5432',
+
+
     }
 }
 
